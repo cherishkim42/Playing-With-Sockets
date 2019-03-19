f74450c5033e56d8389e60f72ad1d9dfe2bf56c0
ilikecoffee18 <cherish.kim@students.makeschool.com>
2019-03-19 09:21:23 -0700

    init commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('index.handlebars')
+})
+
+server.listen('3000', function() {
+  console.log('Server listening on Port 3000!')
+})
