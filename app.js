9f36ea6024dab19116fdc232abefaf14dabe0f4d
ilikecoffee18 <cherish.kim@students.makeschool.com>
2019-03-19 09:26:20 -0700

    integrated socket.io

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 
+const io = require('socket.io')(server)
+io.on('connection', (socket) => {
+  console.log('🔌new user connected woo🔌')
+})
+
 const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs())
 app.set('view engine', 'handlebars')
+app.use('/public', express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index.handlebars')
