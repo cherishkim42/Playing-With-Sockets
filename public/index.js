923a17a41b422ff70ae649620f99afca6f9b4292
ilikecoffee18 <cherish.kim@students.makeschool.com>
2019-03-19 18:42:15 -0700

    users can join chat & send msgs. the msgs render

--- public/index.js
+++ public/index.js
@@ -2,6 +2,9 @@ $(document).ready(() => {
   //Connect to the socket.io server
   const socket = io.connect()
 
+  //Keep track of current user
+  let currentUser;
+
   $('#createUserBtn').click((e) => {
     e.preventDefault()
     if ($('#usernameInput').val().length > 0) {
@@ -9,9 +12,27 @@ $(document).ready(() => {
       //Emit to the server the new user!
       //"new user" is what's known as a NEW EVENT
       socket.emit('new user', $('#usernameInput').val())
+      //Save the current user when created
+      currentUser = $('#usernameInput').val();
       $('.usernameForm').remove()
       //Make main page visible by updating display of .mainContainer from none to flex
       $('.mainContainer').css('display', 'flex')
+    }
+  })
+
+  //We need this button to emit a NEW MESSAGE event to the server and have the server emit a NEW MESSAGE event to all connected clients
+  $('#sendChatBtn').click((e) => {
+    e.preventDefault()
+    //Get the msg txt value
+    let message = $('#chatInput').val()
+    //Make sure it's not empty
+    if(message.length > 0){
+      //Emit the msg with the current user to the server
+      socket.emit('new message', {
+        sender : currentUser,
+        message : message,
+      })
+      $('#chatInput').val("")
     }
   })
 
@@ -22,4 +43,14 @@ $(document).ready(() => {
     //note the use of append!
     $('.usersOnline').append(`<div class='userOnline'>${username}</div>`)
   })
+
+  //output the new message
+  socket.on('new message', (data) => {
+    $('.messageContainer').append(`
+      <div class='message'>
+        <p class='messageUser'>${data.sender}: </p>
+        <p class='messageText'>${data.message}</p>
+    `)
+  })
+
 })
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,11 +1,16 @@
 module.exports = (io, socket) => {
 
-  
   //This bad boy (slaps) listens sooooo hard for an event called 'new user'. He is a MATCHING SOCKET LISTENER!
   //Listen for 'new user' socket emits
   socket.on('new user', (username) => {
     console.log(`❤️${username} has joined the chat❤️`)
     //Emits username to all clients on connection because io.emit, as opposed to socket.emit, which emits only to the client that sent the 'new user'
     io.emit('new user', username)
+  })
+
+  socket.on('new message', (data) => {
+    //Send that data back to all clients
+    console.log(`🎤 ${data.sender}: ${data.message} 🎤`)
+    io.emit('new message', data)
   })
 }
